585cc0573d29a582c96ed3422a4de97d3a482715
Moumita Murmu <69195751+MoumitaMurmu@users.noreply.github.com>
2023-05-23 09:05:23 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,24 @@
 
+// import React from "react";
+// import './../styles/App.css';
+
+// const App = () => {
+//   return (
+//     <div>
+//         {/* Do not remove the main div */}
+//     </div>
+//   )
+// }
+
+// export default App
+
+
 import React from "react";
 import './../styles/App.css';
-
+import ParentComponent from "./Parent"
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
+        <ParentComponent/>
   )
 }
 
